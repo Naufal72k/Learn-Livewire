6283e1729f0d00469240d2b7ab009caa1ee4dd4f
Naufal72k <naufal72k@gmail.com>
2025-09-18 20:02:57 +0800

    sampe tampilkan dan create aja dulu, bsk lanjut hapus dan edit

--- Main/resources/views/livewire/todo-list.blade.php
+++ Main/resources/views/livewire/todo-list.blade.php
@@ -1,15 +1,25 @@
 <div class="p-4">
     <h2 class="text-xl font-bold mb-3">To-Do List 📝</h2>
 
+    <form wire:submit ="addTask">
+        <input type="text" wire:model="title" placeholder="Tulis task baru.....">
+        <button type="submit">Tambah</button>
+    </form>
+
     <ul class="list-disc pl-6">
         @foreach ($tasks as $task)
-            <li>{{ $task['title'] }}
-                @if ($task['done'])
+            <li>{{ $task->title }}
+                @if ($task->done)
                     ✅
                 @else
                     ⌛
                 @endif
             </li>
+        @endforeach
+    </ul>
+    <ul>
+        @foreach ($tasks as $task)
+            <li>{{ $task }}</li>
         @endforeach
     </ul>
 </div>
