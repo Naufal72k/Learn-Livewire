176c617d04332a01d62efd92edf788069328b867
Naufal72k <naufal72k@gmail.com>
2025-09-18 19:40:06 +0800

    buat data di controller

--- Main/app/Livewire/TodoList.php
+++ Main/app/Livewire/TodoList.php
@@ -6,8 +6,14 @@ use Livewire\Component;
 
 class TodoList extends Component
 {
+    public $tasks = [
+        ['id' => 1, 'title' => 'Belajar Livewire' , 'done' => false],
+        ['id' => 2, 'title' => 'Ngoding To-do list' , 'done' => true],
+        ['id' => 3, 'title' => 'ngopi dulu' , 'done' => false]
+    ];
     public function render()
     {
+
         return view('livewire.todo-list');
     }
 }
--- Main/resources/views/livewire/todo-list.blade.php
+++ Main/resources/views/livewire/todo-list.blade.php
@@ -1,3 +1,15 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+<div class="p-4">
+    <h2 class="text-xl font-bold mb-3">To-Do List 📝</h2>
+
+    <ul class="list-disc pl-6">
+        @foreach ($tasks as $task)
+            <li>{{ $task['title'] }}
+                @if ($task['done'])
+                    ✅
+                @else
+                    ⌛
+                @endif
+            </li>
+        @endforeach
+    </ul>
 </div>
